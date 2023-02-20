46b1918d533f7f64a0006ba0744aea424bf271ee
Vinicius Fernandes Fogaça <viniciusfernandes38@gmail.com>
2023-02-20 16:45:32 -0300

    working with events

---
+++ src/app/components/events/events.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent {
+  show:boolean = false;
+
+  showMessage(): void {
+    this.show = !this.show; //toggle
+    
+  }
+
+}
