93564bcdb82f3bb15dc298fa543322067d7dcf1a
Vinicius Fernandes Fogaça <viniciusfernandes38@gmail.com>
2023-02-22 18:34:45 -0300

    working with interface

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
-  animals = [
-    {name: "Lost", type: "Dog"},
-    {name: "Lili", type: "Cat"},
-    {name: "Bob", type: "Horse"}
-  ]
+  animals: Animal[] = [
+    {name: "Lost", type: "Dog", age: 12},
+    {name: "Lili", type: "Cat", age: 3},
+    {name: "Bob", type: "Horse", age: 5}
+  ];
+
+  animalDetails = '';
+
+  showAge(animal: Animal){
+    this.animalDetails = `The pet ${animal.name} is ${animal.age} years old!`;
+  }
 
 }
